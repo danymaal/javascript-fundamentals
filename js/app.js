f30851ffa37c0b873962da2d616e87a9a14a567d
Dany <deeelorxxd@gmail.com>
2022-02-05 21:40:30 +0200

    hi

---
+++ js/app.js
@@ -0,0 +1,68 @@
+// alert(2);
+
+// 1. Variables
+
+const firstName = 'Dany';
+let age = 26;
+
+age = 17;
+
+console.log(age);
+
+// 2. Mutation
+
+console.log('His name is ' + firstName + ' he is ' + age);
+
+// const giveName = prompt('Enter your name');
+
+console.log('The name of the person is ' + firstName);
+
+// 3. Operators
+
+let year = 2022;
+const birthYear = 2004;
+
+const isNow = year - birthYear;
+console.log(isNow);
+
+const a = 10;
+const b = 5;
+
+let c = 32;
+
+// c = c + a
+c += a;
+// c -= a; = c = c - a
+// c *= a; = c = c * a
+// c /= a; = c = c / a
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.log(year++);
+
+// year++ = year + 1
+// year-- = year - 1
+
+console.log(++year);
+console.log(--year);
+
+console.log(c);
+
+// 4. Data types
+const isEntrepreneur = true;
+const sex = 'male';
+const goose = 3;
+let x;
+
+// console.log(typeof isEntrepreneur);
+console.log(typeof isEntrepreneur);
+console.log(typeof sex);
+console.log(typeof goose);
+console.log(typeof x);
+console.log(null);
+
+// 5. Operator precedende
+
