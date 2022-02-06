c0ca2faefe638c1029ced450058ae8337f7f9694
Dany <deeelorxxd@gmail.com>
2022-02-06 19:05:33 +0200

    hi

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Number
+
+const num = 42; // integer
+const float = 42.37; // float
+const pow = 10e3; //
+
+console.log(pow);
+console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER);
+// In console Number. for all type of numbers
+
+console.log('Math.pow 53', Math.pow(2, 53) - 1);
+
+console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER);
+
+console.log('MAX_VALUE', Number.MAX_VALUE);
+
+console.log('MIN_VALUE', Number.MIN_VALUE);
+
+console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY);
+console.log('POSITIVE_INFINITY', Number.NEGATIVE_INFINITY);
